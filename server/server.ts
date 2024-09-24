4dc72549738460a4aac1fcbbe2b6fee9b247c26e
Or Shavit Mellibovsky <oranus541@gmail.com>
2024-09-24 12:45:30 +0300

    version 1.2.2 (json fix)

--- server/server.ts
+++ server/server.ts
@@ -8,15 +8,18 @@ import userRoutes from './routes/userRoutes';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'  // Only allow requests from this specific origin
+}));
+ // if not enabled the browser will block requests between port 3001 to port 3000
 
 // Connect to MongoDB using Mongoose
 const MONGO_URI = 'mongodb://localhost:27017/mydatabase';
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions)
+mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Failed to connect to MongoDB:', error));
 
-// Use Routes
+  // Use Routes
 app.use('/api', authRoutes);       //  (register, login)
 app.use('/api', taskRoutes);       //  task management (add, update, delete)
 app.use('/api', userRoutes);       //  fetching user tasks
